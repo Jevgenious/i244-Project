e230cd39d496e1d43cdc8e135f08e2bfc4557711
Jevgenious <jevgenious@gmail.com>
2017-06-01 21:52:19 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -37,23 +37,21 @@ echo "<a href='logout.php'>Logout</a>";
 <body>
 <head>
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" >
 
 <link rel="stylesheet" href="styles.css" >
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 
 <div class="container">
       <form class="form-signin" method="POST">
         <h2 class="form-signin-heading">Please Login</h2>
-        <div class="input-group">
-	  <input type="text" name="username" class="form-control" placeholder="Username" required>
-	</div>
+		<input type="text" name="username" class="form-control" placeholder="Username" required>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
--- register.php
+++ register.php
@@ -2,16 +2,10 @@
 <html>
 <body>
 <head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
-
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" >
 <link rel="stylesheet" href="styles.css" >
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <?php
 	require('connect.php');
@@ -44,12 +38,12 @@
 <div class="container">
       <form class="form-signin" method="POST">
         <h2 class="form-signin-heading">Please Register</h2>
-        <div class="input-group">
-	  <input type="text" name="username" class="form-control" placeholder="Username" required>
-	</div>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+	    <input type="text" name="username" class="form-control" placeholder="Username" required>
+		
+        <!--<label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>-->
         <label for="inputPassword" class="sr-only">Password</label>
+		
         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
         <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
